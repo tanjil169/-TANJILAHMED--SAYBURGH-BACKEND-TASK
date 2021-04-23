026e58eb2c49fff71006c94d0ee95fa9cf8c02bd
tanjil169 <tanjil169@gmail.com>
2021-04-23 02:23:27 -1200

    simple change

--- blog/resources/views/layouts/master.blade.php
+++ blog/resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300;400;500&display=swap" rel="stylesheet">
 
-    <title>Document</title>
+    <title>Blogging Site</title>
 
     <style>
         body {
